e628e7f41f99ededc0bd67fe85746a2e961d9c82
Skaidrius <skaidrius@gmail.com>
2015-12-14 09:26:50 +0000

    working on adding holidays

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 function CreateTable() {
       var tablecontents = "", 
-      days = ["P", "A", "T", "K", "Pn", "Š", "S"], 
+      //   get current year from select
+      year = document.getElementById("year").value,
+      months = ["Sausis", "Vasaris", "Kovas", "Balandis", "Gegužė", "Birželis", "Liepa", "Rugpjūtis", "Rugsėjis", "Spalis", "Lapkritis", "Gruodis"], 
+      days = ["P", "A", "T", "K", "Pn", "Š", "S"],  
       day_class,
-      months = ["Sausis", 
-    "Vasaris", "Kovas", "Balandis", "Gegužė", "Birželis", "Liepa", "Rugpjūtis", "Rugsėjis", "Spalis", "Lapkritis", "Gruodis"],
-      temp_year =2016,
       days_in_month = [];
-      
-    //   get current year from select
-      temp_year = document.getElementById("year").value;
-      year=temp_year;
 
-    //   create array with month days count
+    //  Create and fill calendar
+        // create array with month days count
       for (var i = 0; i < 12; i++) {
         var num_of_days = new Date(year, i+1, 0).getDate();
         days_in_month.push(num_of_days);
       }
         
-    // fill month  
+        // fill month  
       for (var i = 0; i < 12; i++) {
         tablecontents += "<table class='months'>";
-        tablecontents += "<theader>"+"<tr class='month_name'>"+"<th colspan=7>"+months[i]+ "</th>"+"</tr>"+"</theader>";
+        tablecontents += "<thead>"+"<tr class='month_name'>"+"<th colspan=7>"+months[i]+ "</th>"+"</tr>"+"</thead>";
         tablecontents += "<tbody>";
 
-        // fill week kays names
+        // fill week days names
         tablecontents += "<tr>"; 
         for (var d = 0; d <=6 ; d++) {	
           day_class = d > 4 ? "<td class='day_name day_weekend'>" : "<td class='day_name'>";
@@ -35,11 +32,11 @@ function CreateTable() {
         var starting_day = new Date(year, i, 0).getDay();
         var day = 1; 
         
-        // set week number
+            // set week number (column) cycle
         for (var weekNum = 0; weekNum <= 6; weekNum ++) {
           tablecontents += "<tr>"; 
           
-        //   set days during week
+                // calendar view / place days (empty rows) during week 
           for (var weekDay = 0; weekDay <= 6; weekDay ++) {
             day_class = weekDay > 4 ? "<td class='day_weekend'>" : "<td class='day'>";
             if (day <= days_in_month[i] && (weekNum > 0 || weekDay >= starting_day)) {
@@ -53,28 +50,25 @@ function CreateTable() {
         }
         tablecontents += "</tbody>";
         
-        // insert holidays
-        tablecontents += "<tfooter>";
+        // insert holidays /not working yet!!!
+        tablecontents += "<tfoot>";
         
-        // if (holidays_data[0].year == year){};
-
-
-
-        // for (var month_i = 0; month_i < holidays_data[year].holidays.length; month_i++) {
-        //     if (month_i == i) {
-        //         holiday = holidays_data[year].holidays[month_i+1].day + " d. "+ holidays_data[year].holidays[month_i+1].description;
+        // if (holidays_data[0].year == year && holidays_data[0].holidays[i].month == i+1){
+        //     tablecontents += "<tr><th colspan=7>Nedarbo dienos:</th></tr>";
+        //     for (var month_i= 0; month_i < holidays_data[0].holidays.length; month_i++){
         //         tablecontents += "<tr>";
-        //         tablecontents += "<th colspan=7>Nedarbo dienos:</th>";
-        //         tablecontents += "<td>" + holiday + "</td></tr>";
+        //         tablecontents += "<td colspan=7>" + holidays_data[0].holidays[month_i].description + "</td>";
+        //         tablecontents += "</tr>";
         //     }
+            //     alert(holidays_data[year_i].year);
+                // if (holidays_data[year_i].year == year && holidays_data[year_i].holidays[month_i] == i){
+                //     tablecontents += "<th colspan=7>Nedarbo dienos:</th>";
         // }
-
-        // tablecontents += "</tr>";
-        tablecontents += "</tfooter>";
+        // alert(holidays_data[ye].year);
+        tablecontents += "</tfoot>";
         tablecontents += "</table>";
-    // }
+      } //end of month cycle
+
 
 document.getElementById("tablespace").innerHTML = tablecontents;
 }
-
-
--- data.js
+++ data.js
@@ -1,84 +1,76 @@
 var holidays_data = [
-    {   
-        year : 2016, 
-        holydays : [ 
-            {
+    {
+        universalHolidays: [ 
+            { 
                 month:1, 
-                day:1, 
+                day:1,  
                 description: "Naujieji Metai"
-            }, 
-            {
+            }, { 
                 month:2, 
                 day:16, 
-                description: "Lietuvos valstybės atkūrimo diena"
-            }, 
-            {
-                month: 3, 
+                description: "Lietuvos valstybės atkūrimo diena"  
+            }, { 
+                month:3, 
                 day: 11, 
-                description: "Lietuvos nepriklausomybės atkūrimo diena"
-            },  
-            {
-                month:3, 
-                day:27, 
-                description: "Šv. Velykos"
-            }, 
-            {   
-                month:3, 
-                day:28, 
-                description: "Šv. Velykos"
-            }, 
-            {   
+                description: "Lietuvos nepriklausomybės atkūrimo diena" 
+            }, {
                 month:5, 
-                day:1, 
-                description: "Tarptautinė darbininkų diena"
-            },
-            {   
-                month:5, 
-                day:1, 
-                description: "Motinos diena"
-            },
-            {   
-                month:6, 
-                day:5, 
-                description: "Tėvo diena"
-            }, 
-            {   
+                day:1,  
+                description: "Tarptautinė darbininkų diena" 
+            }, { 
                 month:6, 
                 day:24, 
-                description: "Rasos ir Joninių diena"
-            },
-            {   
+                description: "Rasos ir Joninių diena" 
+            }, { 
                 month:7, 
-                day:6, 
-                description: "Valstybės (Lietuvos karaliaus Mindaugo karūnavimo) diena"
-            },
-            { 
+                day:6,  
+                description: "Valstybės (Lietuvos karaliaus Mindaugo karūnavimo) diena" 
+            }, { 
                 month:8, 
                 day:15, 
-                description: "Žolinės (Švč. Mergelės Marijos ėmimo į dangų diena)"
-            },
-            { 
+                description: "Žolinės (Švč. Mergelės Marijos ėmimo į dangų diena)" 
+            }, { 
                 month:11, 
-                day:1, 
-                description: "Visų Šventųjų diena"
-            },
-            { 
+                day:1,  
+                description: "Visų Šventųjų diena" 
+            }, { 
                 month:12, 
                 day:24, 
-                description: "Kūčios"        
-            },
-            {   month:12, 
+                description: "Kūčios"  
+            }, { 
+                month:12, 
                 day:25, 
-                description: "Šv. Kalėdos"
-            },
-            {   month:12, 
+                description: "Šv. Kalėdos" 
+            }, { 
+                month:12, 
                 day:26, 
-                description: "Šv. Kalėdos"
+                description: "Šv. Kalėdos" 
             } 
-        ] 
-    },
-    {
-        year: 2017, 
-        holidays: []
-    }
+        ],
+        yearly: [
+            {
+                year: 2016,
+                specialHolidays: [ 
+                    { 
+                        month:3, 
+                        day:27, 
+                        description: "Šv. Velykos" 
+                    },{ 
+                        month:3, 
+                        day:28, 
+                        description: "Šv. Velykos" 
+                    }, { 
+                        month:5, 
+                        day:1,  
+                        description: "Motinos diena" 
+                    }, { 
+                        month:6, 
+                        day:5,  
+                        description: "Tėvo diena" 
+                    }
+                ]
+        }, {
+                year: 2017, 
+                specialHolidays: [] }
+        ] }
 ];
